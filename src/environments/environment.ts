9aeafbb53d5a3e9e9c1ae2f8197f8b9c5bbc3137
kev127 <kelvinkeya125@gmail.com>
2020-10-03 12:07:51 +0300

    add key to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  key : 'bb74c565705f92dc6e86ddbf2be0c283546fa6b1'
 };
 
 /*
