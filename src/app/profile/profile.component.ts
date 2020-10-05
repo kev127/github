e81a9d145c595afdec1f92f5bca7f03415fe3b53
kev127 <kelvinkeya125@gmail.com>
2020-10-05 08:59:57 +0300

    add component profile

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user: any;
+  userRepos:any;
+
+  constructor(private profileService: ProfileService) { 
+
+  }
+
+  findUser () {
+    this.profileService.UpdateUser(this.user);
+
+    this.profileService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+
+    this.profileService.getUserRepos().subscribe(repos => {
+      console.log(repos);
+      this.userRepos = repos;
+    })
+  }
+
+  ngOnInit() {
+    this.findUser()
+  }
+
+}
