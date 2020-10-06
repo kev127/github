2bf6c74336d953e1c9c2830aa83bc1ae42c19502
kev127 <kelvinkeya125@gmail.com>
2020-10-06 10:03:17 +0300

    add class repo

---
+++ src/app/repo.ts
@@ -0,0 +1,13 @@
+export class Repo {
+    constructor(
+        public name:string,
+        public description:string,
+        public repos:string,
+        public language :string,
+        public html_url :string,
+        public created_at:string,
+
+    ){
+        
+    }
+}
