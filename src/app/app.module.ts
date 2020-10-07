38ed88f8cedabde1cda7d18717f456f3f27588ff
kev127 <kelvinkeya125@gmail.com>
2020-10-07 13:59:20 +0300

    deleted repo component and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { RepoComponent } from './repo/repo.component';
 import { ProfileService } from './profile.service';
-import { RepoDirective } from './repo.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    RepoComponent,
-    RepoDirective,
   ],
   imports: [
     BrowserModule,
--- src/app/repo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RepoDirective } from './repo.directive';
-
-describe('RepoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RepoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
